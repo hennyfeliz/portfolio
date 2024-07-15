066b9d71a67309156b58a8bbae066908d508f888
Henny Feliz <mingito.2017@gmail.com>
2024-07-15 04:28:51 -0400

    Arrow icon component

---
+++ portfolio/src/assets/icons/Arrow.jsx
@@ -0,0 +1,11 @@
+export default function Arrow() {
+  return <svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path d="M6 12H18M18 12L13 7M18 12L13 17"
+        stroke="var(--dark)"
+        strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+    </g>
+  </svg>
+}
