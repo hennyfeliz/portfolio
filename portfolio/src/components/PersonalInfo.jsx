a3a7731d6b96a95bed148d85ecc08caa07fa0f76
Henny Feliz <mingito.2017@gmail.com>
2024-07-16 01:48:14 -0400

    Personal info component

---
+++ portfolio/src/components/PersonalInfo.jsx
@@ -0,0 +1,19 @@
+const PersonalInfo = () => {
+  return (
+    <div>
+      <p><div className="personal-content__title" data-v-639074ca="">
+        <p className="personal-content__text" data-v-639074ca="">Hi, I&apos;m</p>
+        <div className="title-content" data-v-639074ca="">
+          <ul className="title-list more-space" data-v-639074ca="">
+            <li className="title-list__item" data-v-639074ca="">Henny M. Feliz!</li>
+            <li className="title-list__item" data-v-639074ca="">
+              a Software Developer
+            </li>
+          </ul>
+        </div>
+      </div>I&apos;m fullstack web developer from Dominican Republic with +2 years of experience. I&apos;ve many experience in Java technologies and React/React Native.</p>
+    </div>
+  )
+}
+
+export default PersonalInfo
