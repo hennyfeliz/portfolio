6221ca49b50cb04c4c6fb6e23f81c8b09d946b45
Henny Feliz <mingito.2017@gmail.com>
2024-07-16 01:47:58 -0400

    experience componente

---
+++ portfolio/src/components/Experience.jsx
@@ -0,0 +1,22 @@
+import '../../styles/Experience.css'
+
+const Experience = () => {
+  return (
+    <ul className="ugnized-list">
+      <li>
+        <div></div>
+        <time className='time_'>Nov 2022 - Present</time>
+        <div>
+          <span className="title_experience">Software Developer</span>
+          <span className='text_s'>at Banreservas - The most important Bank in Dom Rep</span>
+        </div>
+        <p className='text_s'>+2 years of experience in software development as a Software Developer at
+          BanReservasRD, developing web applications with JavaServer Faces
+          (JSF). I also worked guiding the user, listening to their feedback and
+          implementing new features upon request.</p>
+      </li>
+    </ul>
+  )
+}
+
+export default Experience
