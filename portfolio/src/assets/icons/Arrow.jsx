5480c897988a4bdfb7cceb11d4014e8a6052b4f5
Henny Feliz <mingito.2017@gmail.com>
2024-07-18 00:02:28 -0400

    Arrow icon component changes

--- portfolio/src/assets/icons/Arrow.jsx
+++ portfolio/src/assets/icons/Arrow.jsx
@@ -1,10 +1,10 @@
 export default function Arrow() {
-  return <svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  return <svg width="18px" height="18px" className="icon__" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
     <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
     <g id="SVGRepo_iconCarrier">
       <path d="M6 12H18M18 12L13 7M18 12L13 17"
-        stroke="var(--dark)"
+        stroke="var(--dark--dk)"
         strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
     </g>
   </svg>
