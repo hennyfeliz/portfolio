44548a193cbfae2885968b9f42fcdf59f11e7b83
Henny Feliz <mingito.2017@gmail.com>
2024-07-19 00:07:43 -0400

    base initial projects js file for base project component js data

---
+++ portfolio/src/utils/initialProjects.js
@@ -0,0 +1,45 @@
+export default [
+  {
+    id: 1,
+    name: "instagram-clone",
+    description: "Instagram clone with React Native",
+    topics: ["React Native", "Node.js", "MongoDB"],
+    html_url:
+      "https://github.com/hennyfeliz/react-native-projects/tree/main/instagram-clone",
+  },
+  {
+    id: 2,
+    name: "school-management-system",
+    description: "A web app for managing a students, teachers, and classes",
+    topics: ["React", "Quarkus", "SQL Server"],
+    html_url: "https://github.com/hennyfeliz/students-management",
+  },
+  {
+    id: 3,
+    name: "working-with-images",
+    description: "A web app for uploading and displaying images",
+    topics: ["React", "Quarkus", "SQL Server"],
+    html_url: "https://github.com/hennyfeliz/working-with-images",
+  },
+  {
+    id: 4,
+    name: "ecommerce",
+    description: "A web app for managing an ecommerce",
+    topics: ["React", "Spring-Boot", "SQL Server"],
+    html_url: "https://github.com/hennyfeliz/ecomerce",
+  },
+  {
+    id: 5,
+    name: "xls-processor",
+    description: "A web app for saving excel files content into a database",
+    topics: ["React", "Spring-Boot", "Docker"],
+    html_url: "https://github.com/hennyfeliz/xls-processor",
+  },
+  {
+    id: 6,
+    name: "notes-app",
+    description: "A web app for saving, editing and deleting notes",
+    topics: ["Node.js", "MongoDB", "Express"],
+    html_url: "https://github.com/hennyfeliz/notes-app-nodejs-mongodb",
+  },
+];
